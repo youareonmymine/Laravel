fae2c557ca4837d28233ba1d3410569e5accfec8
youareonmymine <74822268+youareonmymine@users.noreply.github.com>
2021-10-12 14:32:01 +0800

    Update Controller.php

--- app/Admin/Controllers/Controller.php
+++ app/Admin/Controllers/Controller.php
@@ -2,22 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * +----------------------------------------------------------------------+
- * |                          ThinkSNS Plus                               |
- * +----------------------------------------------------------------------+
- * | Copyright (c) 2016-Present MeetingChuangXiang Technology Co., Ltd.     |
- * +----------------------------------------------------------------------+
- * | This source file is subject to enterprise private license, that is   |
- * | bundled with this package in the file LICENSE, and is available      |
- * | through the world-wide-web at the following url:                     |
- * | https://github.com/slimkit/plus/blob/master/LICENSE                  |
- * +----------------------------------------------------------------------+
- * | Author: Slim Kit Group <master@Meetingcx.com>                          |
- * | Homepage: www.thinksns.com                                           |
- * +----------------------------------------------------------------------+
- */
-
 namespace Meeting\Plus\Admin\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
